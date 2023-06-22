3c4fdfd485729fd72267107af823671b1010d026
allenthreee <59171742+allenthreee@users.noreply.github.com>
2023-06-22 14:58:13 +0800

    Add files via upload

---
+++ docker_run_ego_cpp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+sudo docker run \
+  $run_args \
+  -it \
+  --network host \
+  --privileged \
+  --volume=$XSOCK:$XSOCK:rw \
+  --volume=$XAUTH:$XAUTH:rw \
+  --env="XAUTHORITY=${XAUTH}" \
+  --env DISPLAY=$DISPLAY \
+  --env TERM=xterm-256color \
+  -v /dev:/dev \
+  --name "ego_cpp_dragon_boat" \
+  allenthreee/ego_cpp:dragon_boat_festival \
+  /bin/bash
